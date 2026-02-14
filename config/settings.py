ca19dffe00923c3d8bb038e64261f69de3297f56
P-Alakara <alakara11728@gmail.com>
2026-02-14 22:48:03 +0300

    Add signals to automatically create profiles

--- config/settings.py
+++ config/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
-    'profiles',
+    'profiles.apps.ProfilesConfig',
     'jobs',
     'dashboard',
     'core',
