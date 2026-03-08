90c46b1c977887e290bb11a535e9cd203f779b47
P-Alakara <alakara11728@gmail.com>
2026-03-08 22:42:58 +0300

    prepare for render deployment

---
+++ config/settings/production.py
@@ -0,0 +1,32 @@
+from .base import *
+import os
+import dj_database_url
+
+DEBUG = False
+
+SECRET_KEY = os.environ['SECRET_KEY']
+
+ALLOWED_HOSTS = [os.environ.get('RENDER_EXTERNAL_HOSTNAME', 'localhost')]
+
+DATABASES = {
+    'default': dj_database_url.config(
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
+}
+
+MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOWED_ORIGINS = [
+    os.environ.get('FRONTEND_URL', ''),
+]
+
+from datetime import timedelta
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
+}
